b0c5655444339b507d1846dbc428774e91dd9c79
Alex Walton <alexjameswalton@gmail.com>
2021-05-25 13:38:01 +0530

    lines added to jokes object

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
+const jokes = {
 
-const lilPeteyQ = [
-    "Lil Petey:  What is a chickens favorite food?",
-    "Lil Petey:  What do you call a sheep with no legs?",
-    "Lil Petey:  What do you call a fish with no eyes?", 
-    "Lil Petey:  What does a frog have for breakfast?",
-    "Lil Petey:  What do you get if you mix two people and two more people?",
-    "Lil Petey:  What did the French-man say when he agreed twice?"
-];
+    lilPeteyQ : [
+        "Lil Petey:  What is a chickens favorite food?",
+        "Lil Petey:  What do you call a sheep with no legs?",
+        "Lil Petey:  What do you call a fish with no eyes?", 
+        "Lil Petey:  What does a frog have for breakfast?",
+        "Lil Petey:  What do you get if you mix two people and two more people?",
+        "Lil Petey:  What did the French-man say when he agreed twice?"
+    ],
 
-const peteyQ = [
-    "Petey:  I don't know",
-    "Petey:  I give up",
-    "Petey:  This won't be funny",
-    "Petey:  Will you stop?",
-    "Petey:  Please be quiet",
-    "Petey:  ARGHHHHHH"
-];
+    peteyQ : [
+        "Petey:  I don't know",
+        "Petey:  I give up",
+        "Petey:  This won't be funny",
+        "Petey:  Will you stop?",
+        "Petey:  Please be quiet",
+        "Petey:  ARGHHHHHH"
+    ],
 
-const lilPeteyA = [
-    "Lil Petey:  Poo",
-    "Lil Petey:  A diarrhea cloud",
-    "Lil Petey:  A wee-wee",
-    "Lil Petey:  Frog poo",
-    "Lil Petey:  Poo-poo",
-    "Lil Petey:  Wee-wee"
-];
+    lilPeteyA : [
+        "Lil Petey:  Poo",
+        "Lil Petey:  A diarrhea cloud",
+        "Lil Petey:  A wee-wee",
+        "Lil Petey:  Frog poo",
+        "Lil Petey:  Poo-poo",
+        "Lil Petey:  Wee-wee"
+    ]
+};
 
-console.log(lilPeteyQ[5])
-console.log(peteyQ[5])
-console.log(lilPeteyA[5])
+console.log(jokes.lilPeteyA[0])
